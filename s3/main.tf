42f4e8c54f8ea08124ea3e7bc19aed85346eef91
hsh0367 <hsh0367@gmail.com>
2025-01-10 19:12:54 +0900

    todo 추가

--- s3/main.tf
+++ s3/main.tf
@@ -12,6 +12,8 @@ resource "aws_s3_bucket" "new_bucket" {
   }
 }
 
+# Todo: 버킷 acl 구성 및 정책 추가
+# Todo: 태그로 연결된 iam user 생성 및 지정된 정책 arn 연결
 
 # # 버킷에 대한 public access block 구성을 변경해준다.
 # resource "aws_s3_bucket_public_access_block" "public-access" {
